4c07e477eee505ee7320db99fb43611756230dab
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-12-11 11:17:47 +0100

    Create session_data table

--- sql/database.sql
+++ sql/database.sql
@@ -4,8 +4,15 @@ GRANT CONNECT ON DATABASE login_form TO krzysiek;
 
 CREATE TABLE
 IF NOT EXISTS login_data (
+    user_id SERIAL PRIMARY KEY,
     user_login TEXT,
     user_password TEXT
+);
+
+CREATE TABLE
+IF NOT EXISTS session_data (
+    user_id INTEGER REFERENCES login_data(user_id),
+    session_id TEXT
 );
 
 INSERT INTO login_data (user_login, user_password)
