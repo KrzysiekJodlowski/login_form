a4ad1edf44f1daa5fef5eaf7e72cdd56a4591685
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-12-11 09:57:15 +0100

    Create database with login_data table with database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,3 +1,21 @@
 -- create database and table with users
+CREATE DATABASE login_form WITH OWNER = krzysiek;
+GRANT CONNECT ON DATABASE login_form TO krzysiek;
+
+CREATE TABLE
+IF NOT EXISTS login_data (
+    user_login TEXT,
+    user_password TEXT
+);
+
+INSERT INTO login_data (user_login, user_password)
+VALUES
+('admin', 'admin123'),
+('mentor', 'mentor123'),
+('student', 'student123');
 
 -- search entry with specific login and password
+SELECT * FROM login_data
+WHERE
+user_login=? AND user_password=?;
+
